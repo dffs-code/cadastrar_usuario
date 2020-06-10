c2eaa4ed48339900c27e3eee408e246ae8c8236b
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2020-06-10 11:03:08 -0300

    added input on array working with state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Lista from './components/Lista/Lista';
-import Input from './components/Input/Input'
 
 function App() {
   return (
     <>
-      <Input />
       <Lista />
     </>
   );
--- src/components/Lista/Lista.js
+++ src/components/Lista/Lista.js
@@ -1,29 +1,62 @@
 import React, { Component } from 'react';
-export const user = [{
+import './Lista.css'
+
+const user = [{
   nome: 'daniel',
   profissao: 'estudante'
   },
   {
   nome: 'jeferson',
-  profissao: 'programador'}
-];
+  profissao: 'programador'
+  }
+]
 
 export default class Lista extends Component{
+    state = {
+        user
+      }
     render(){
         let rows = []
         for(let i=0; i<user.length; i++){
           rows.push( 
             <li>
-              {user[i].nome} é {user[i].profissao}
+              {this.state.user[i].nome} é {this.state.user[i].profissao}
             </li>
           )
         }
         return (
+          <>
+          <div className='input'>
+              <label htmlFor="nome">Nome:</label>
+              <input type='text' placeholder='Nome' id='nome'></input>
+              <label htmlFor="profissao">Profissão:</label>
+              <input type='text' placeholder='Profissão' id='profissao'></input>
+
+              <button className="btn waves-effect waves-light" type="submit" name="action" onClick= {() => {
+                  const userInserted = {
+                      nome: document.getElementById('nome').value,
+                      profissao: document.getElementById('profissao').value
+                  }
+                  user.push(userInserted);
+                  this.setState(user);
+
+                  document.getElementById('nome').value = ''
+                  document.getElementById('profissao').value = ''
+              }}>Cadastrar
+                  <i className="material-icons right">send</i>
+              </button>
+          </div>
+
+          {/* modificar depois para deixar como tabela 
+              implementar remoção de usuário
+          */}
           <div>
             <ul>
               {rows}
             </ul>
           </div>
+          </>
+
       )
     }
 }
