1c7b00ad18d5f6e257586a12804ce7753e696aae
Daniel Formigoni <d.soares@aluno.ifsp.edu.br>
2020-06-11 15:34:52 -0300

    Updates

---
+++ precache-manifest.a725bce958efcbdd1edfca2b7e239698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee4fcc454a5590cf7476d0ec903508e",
+    "url": "/cadastrar_usuario/index.html"
+  },
+  {
+    "revision": "74c2907dddae94c5dc6f",
+    "url": "/cadastrar_usuario/static/css/main.3c791f9a.chunk.css"
+  },
+  {
+    "revision": "fd69e869e440797803bf",
+    "url": "/cadastrar_usuario/static/js/2.2225b769.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cadastrar_usuario/static/js/2.2225b769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c2907dddae94c5dc6f",
+    "url": "/cadastrar_usuario/static/js/main.73d9074b.chunk.js"
+  },
+  {
+    "revision": "3730db4d14dd9775e28c",
+    "url": "/cadastrar_usuario/static/js/runtime-main.b884b800.js"
+  }
+]);
